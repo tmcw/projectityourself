91e42f9c347fa08cbf815828fa21e22fc1f5c8eb
Tom MacWright <tom@macwright.org>
2013-11-11 22:07:47 -0500

    Pre-web-workers

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-;(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var _data;
 var w = 1350,
     h = 675;
@@ -76,6 +75,3 @@ function render() {
 }
 
 d3.select('#render').on('click', render);
-
-},{}]},{},[1])
-;
